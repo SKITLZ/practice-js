baff95267bdbc50dde735a6cf6b5beb6a307f1e9
Oleg Ivinsky <gskitlz@gmail.com>
2024-02-03 11:50:17 +0300

    Add multiplyTwoNumbers

---
+++ basic/multiplyTwoNumbers/_index.test.js
@@ -0,0 +1,43 @@
+const { multiplyTwoNumbers } = require('.');
+
+const mocks = [
+  {
+    a: 0,
+    b: 1,
+    result: 0,
+  },
+  {
+    a: 3,
+    b: 5,
+    result: 15,
+  },
+  {
+    a: -3,
+    b: -2,
+    result: 6,
+  },
+  {
+    a: 18,
+    b: -24,
+    result: -432,
+  },
+  {
+    a: 7,
+    b: 12,
+    result: 84,
+  },
+];
+
+describe('Проверка multiplyTwoNumbers', () => {
+  it('Правильно умножает числа', () => {
+    mocks.forEach(({ a, b, result }) => {
+      expect(multiplyTwoNumbers(a, b)).toEqual(result);
+    });
+  });
+});
+
+// test('Правильно прибавляет единицу к каждому элементу массива', () => {
+//   mocks.forEach(({ arr, result }) => {
+//     expect(getPlusOneArr(arr)).toEqual(result);
+//   });
+// });
---
+++ basic/multiplyTwoNumbers/index.js
@@ -0,0 +1,10 @@
+/**
+ * Функция multiplyTwoNumbers должна возвращать (return) результат умножения двух чисел, переданные в неё первым и вторым аргументами
+ * */
+
+const multiplyTwoNumbers = (a, b) => {
+};
+
+multiplyTwoNumbers(3, 5); // Возвращает 15
+
+module.exports = { multiplyTwoNumbers };
